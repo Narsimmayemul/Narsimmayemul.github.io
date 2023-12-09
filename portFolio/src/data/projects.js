b6b3018601b6ea49a9f1210b36f7ffade7ac3747
Narsimmayemul <narsimmayemul49450@gmail.com>
2023-12-09 20:16:41 +0530

    portfolio

---
+++ portFolio/src/data/projects.js
@@ -0,0 +1,37 @@
+export default function getProjects() {
+    return [
+      {
+       
+        id: 1,
+        image:'MineWine.png',
+        projectTitle: "MineWine Clone",
+        projectInfo:
+         "In this project we have tried to make an Online Wine Ecommerce Website And and added functionally like Signup, Login, Search, Cart page, Payment Page. We used HTML, CSS, JavaScript ,Bootstrap, ES6 Tech Stacks to build this application.",
+
+        techUsed: ["JavaScript", "HTML","CSS", "ES6" , "React"],
+        projectLinkHosted: "https://64dc647497ed712d3fab4cb5--lambent-quokka-6e7d37.netlify.app/index.html",
+        projectLinkGithub: "https://github.com/Vikaschaturvedi167/WineShop_project",
+      },
+      {
+        id: 2,
+        projectTitle: "Boat Clone",
+        image:'Boat.png',
+        projectInfo: "In this project we have tried to make a clone of Boat Ecommerce Website And and added functionally like Signup, Login, Search, Cart page, Payment Page. We used HTML, CSS, JavaScript Tech Stacks.",
+        techUsed: ["HTML","CSS","JavaScript","Netlify"],
+        projectLinkHosted: "https://shivering-need-4074.netlify.app/",
+        projectLinkGithub: "https://github.com/RohanKansara01/boAt-Website-Clone",
+       
+      },
+      {
+        id: 3,
+        projectTitle: "glosure Clone",
+        image:"glosure.png",
+        projectInfo:
+          "This is a Collaborative Project. We clone the glosure Shopping Website and added functionally like Signup, Login, Search, Cart page, Payment Gateway. We used HTML, CSS, JavaScript Tech Stacks.",
+        techUsed: ["JavaScript", "HTML", "CSS","LocalStorage","Netlify"],
+        projectLinkHosted: "https://delightful-phoenix-888d86.netlify.app/",
+        projectLinkGithub: "https://github.com/Saurav9284/Unite-5-Project-Female-Beauty-Product-"
+       
+      }
+    ];
+  }
